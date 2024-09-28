ab59e60a73874654c5e8fdb602c471d218f9d2a9
sea19 <evgenia.ugra@gmail.com>
2024-09-28 23:37:34 +0500

    fix: перенос картинок

--- server/routes/image/[imageName].get.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const imageName = getRouterParam(event, 'imageName');
-    const data = await useStorage('files').getItemRaw(`images:${imageName}`);
-
-    return data;
-});
