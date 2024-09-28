d90674059e44f5acc5596a2119be9583e290a17a
sea19 <evgenia.ugra@gmail.com>
2024-09-28 17:33:20 +0500

    fix: правки в конфиге

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
+    app: {
+        baseURL: process.env.NUXT_APP_BASE_URL ?? '/agency',
+    },
+
     modules: [
         '@nuxt/eslint',
         '@vueuse/nuxt',
